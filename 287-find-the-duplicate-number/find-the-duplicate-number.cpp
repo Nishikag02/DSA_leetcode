a2b4628423d2b376c02e3def5e86322b50204f51
Nishika Gupta <nishikagupta0@gmail.com>
2026-07-21 00:44:45 +0530

    Time: 0 ms (100.00%) | Memory: 65 MB (69.91%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -1,13 +1,22 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        for(int i=0;i<nums.size()-1;i++){
-            if(nums[i]==nums[i+1]){
-                return nums[i];
-            }
+
+        int slow=0;
+        int fast=0;
+
+        while(slow!=fast || slow==0){
+            slow=nums[slow];
+            fast=nums[nums[fast]];
         }
-        return -1;
+
+        slow=0;
+        while(slow!=fast){
+            slow=nums[slow];
+            fast=nums[fast];
+        }
+
+        return slow;
         
     }
 };
