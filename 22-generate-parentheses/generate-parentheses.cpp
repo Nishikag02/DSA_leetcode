062cb7420eb5fb4a43222d9a3d18f3ebd479a1a2
Nishika Gupta <nishikagupta0@gmail.com>
2026-08-28 14:09:57 +0530

    Time: 3 ms (68.05%) | Memory: 10.6 MB (98.46%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -2,24 +2,23 @@ class Solution {
 public:
     vector<string> generateParenthesis(int n) {
 
-        vector<vector<string>> dp(n + 1);
-
-        // 0 pairs
+        vector<vector<string>>dp(n+1);
         dp[0].push_back("");
 
-        for (int i = 1; i <= n; i++) {
+        for(int i=1;i<=n;i++){
 
-            for (int j = 0; j < i; j++) {
+            for(int j=0;j<i;j++){
 
-                for (string left : dp[j]) {
-                    for (string right : dp[i - j - 1]) {
-
-                        dp[i].push_back("(" + left + ")" + right);
+                for(string left:dp[j]){
+                    for(string right:dp[i-j-1]){
+                        dp[i].push_back("("+left+")"+right);
                     }
                 }
             }
         }
 
         return dp[n];
+
+
     }
 };
